05582797f8740d14fd0336ea0b6dcf0bba20a482
juankmiloh <juankmiloh@hotmail.com>
2019-10-16 18:34:32 -0500

    Se cuadra el responsive del login para pantallas grandes

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="login-container">
-
-    <el-row>
-      <el-col :span="8" style="border: 0px solid red; ">
+  <div class="login-container">   
+    <el-row style="border: 0px solid; height: 96%;">
+      <el-col :span="11" style="border: 0px solid red; height: 100%;">
         <el-row>
-          <el-col :span="24" style="border: 0px solid;">
+          <el-col :span="24" style="border: 0px solid; height: 100%;">
             <div class="grid-logoTarifarito">
               <img :src="logTarifarito" alt="Tarifarito" class="imgLogtarifarito">
             </div>
@@ -17,79 +16,135 @@
         </el-row>
       </el-col>
 
-      <el-col :span="16" style="border: 0px solid; z-index: 1;">
+      <el-col :span="13" style="border: 0px solid; z-index: 1; height: 100%;">
 
-        <el-row>
-          <el-col :span="24" style="border: 0px solid;">
+        <el-row style="border: 0px solid yellow; height: 50%;">
+          <el-col :span="24" style="border: 0px solid blue; height: 100%;">
             <div class="grid-logoSuper">
               <img :src="logSuper" alt="Superservicios" class="logoSuper">
               <div class="grid-textSuper">
                 <span style="font-size: 250%;"><b>Superservicios</b></span><br>
                 <span style="font-size: 130%;">Superintendencia de Servicios</span><br>
                 <span style="font-size: 130%;">Públicos Domiciliarios</span>
-              </div>  
+              </div>
             </div>
           </el-col>
         </el-row>
 
-        <el-row> <!-- form -->
-          <el-col :span="24" style="border: 0px solid;">
+        <el-row style="border: 0px solid yellow; height: 50%;"> <!-- form -->
+          <el-col :span="24" style="border: 0px solid red; height: 100%;">
             <div class="grid-form">
               <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
-              <el-form-item prop="username">
-                <span class="svg-container">
-                  <svg-icon icon-class="user" />
-                </span>
-                <el-input
-                  class="input_user"
-                  ref="username"
-                  v-model="loginForm.username"
-                  placeholder="Usuario"
-                  name="username"
-                  type="text"
-                  tabindex="1"
-                  autocomplete="on"
-                />
-              </el-form-item>
-
-              <el-tooltip v-model="capsTooltip" content="Mayúscula" placement="right" manual>
-                <el-form-item prop="password">
+                <el-form-item prop="username">
                   <span class="svg-container">
-                    <svg-icon icon-class="password" />
+                    <svg-icon icon-class="user" />
                   </span>
                   <el-input
-                    :key="passwordType"
-                    ref="password"
-                    v-model="loginForm.password"
-                    :type="passwordType"
-                    placeholder="Contraseña"
-                    name="password"
-                    tabindex="2"
+                    class="input_user"
+                    ref="username"
+                    v-model="loginForm.username"
+                    placeholder="Usuario"
+                    name="username"
+                    type="text"
+                    tabindex="1"
                     autocomplete="on"
-                    @keyup.native="checkCapslock"
-                    @blur="capsTooltip = false"
-                    @keyup.enter.native="handleLogin"
                   />
-                  <span class="show-pwd" @click="showPwd">
-                    <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-                  </span>
                 </el-form-item>
-              </el-tooltip>
 
-              <div style="text-align: right;">
-                <el-button :loading="loading" class="btnLogin" @click.native.prevent="handleLogin">Ingresar</el-button>
-              </div>
+                <el-tooltip v-model="capsTooltip" content="Mayúscula" placement="right" manual>
+                  <el-form-item prop="password">
+                    <span class="svg-container">
+                      <svg-icon icon-class="password" />
+                    </span>
+                    <el-input
+                      :key="passwordType"
+                      ref="password"
+                      v-model="loginForm.password"
+                      :type="passwordType"
+                      placeholder="Contraseña"
+                      name="password"
+                      tabindex="2"
+                      autocomplete="on"
+                      @keyup.native="checkCapslock"
+                      @blur="capsTooltip = false"
+                      @keyup.enter.native="handleLogin"
+                    />
+                    <span class="show-pwd" @click="showPwd">
+                      <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+                    </span>
+                  </el-form-item>
+                </el-tooltip>
 
-            </el-form>
+                <div style="text-align: right;">
+                  <el-button :loading="loading" class="btnLogin" @click.native.prevent="handleLogin">Ingresar</el-button>
+                </div>
+
+              </el-form>
             </div>
           </el-col>
         </el-row>
 
       </el-col>
     </el-row>
+    <div class="footer">
+      <span class="textoFooter"><b>CIAD</b> - Centro de Investigación y Análisis de Datos</span>  
+    </div>
   </div>
 </template>
+
+<style> 
+  /* manejo de responsive para pantallas grandes */
+  
+  /* Extra large devices (large laptops and desktops, 1200px and up) */
+  /* FUNCIONA PARA PANTALLAS MAYORES A 1.300 */
+  @media screen and (min-width: 1300px) {
+
+    /* espacio entre el borde superior y el cuadro de los input */
+    div.grid-form{ 
+      padding-top: 0%;
+      padding-right: 40em;
+    }
+
+    /* propiedades logo tarifarito */
+    div.grid-logoTarifarito {
+      border: 0px solid;
+      text-align: center;
+      padding-top: 35%;
+      padding-left: 75%;
+      z-index: 100;
+      /* width: 100%; */
+      /* height: 95vh; */
+      /* display:flex; */
+      /* align-items: center; */
+      /* margin-left: 30%; */
+    }
+
+    /* propiedades letra T */
+    div.grid-textoTarifarito .letraT{
+      font-size: 284%;
+      position: relative;
+      left: 44px;
+      top: -12px;
+    }
+
+    /* propiedades otras letras */
+    div.grid-textoTarifarito .otrasLetras{
+      border-top: 3.7px solid;
+      font-size: 200%;
+      position: relative;
+      left: 37px;
+      top: -12px;
+    }
+
+    /* propiedades logo superintendencia */
+    div.grid-logoSuper{
+      padding-top: 19%;
+      padding-right: 61%;
+    }
+  }
+
+</style>
 
 <style>
   .imgLogtarifarito{
@@ -100,12 +155,13 @@
 
   .grid-textoTarifarito{
     /* border-top: 2.5px solid; */
-    text-align: center;    
+    text-align: center;
     padding-left: 65%;
     margin-top: -1%;
     z-index: -1;
   }
 
+  /* propiedades letra T */
   .grid-textoTarifarito .letraT{
     font-size: 284%;
     position: relative;
@@ -113,6 +169,7 @@
     top: -16px;
   }
 
+  /* propiedades otras letras */
   .grid-textoTarifarito .otrasLetras{
     border-top: 3.7px solid;
     font-size: 200%;
@@ -121,10 +178,11 @@
     top: -16.5px;
   }
   
+  /* propiedades logo tarifarito */
   .grid-logoTarifarito {
     border: 0px solid;
     text-align: center;
-    padding-top: 43%;
+    padding-top: 30%;
     padding-left: 60%;
     z-index: 100;
     /* width: 100%; */
@@ -138,9 +196,10 @@
     text-align: center;
   }
 
+  /* propiedades logo superintendencia */
   .grid-logoSuper{
-    padding-top: 5%;
-    margin-right: 15%;
+    padding-top: 7%;
+    padding-right: 40%;
   }
 
   .logoSuper{
@@ -149,9 +208,11 @@
     display: block !important;
   }
 
+  /* espacio entre el borde superior y el cuadro de los input */
   .grid-form{
-    padding-top: 3%;
-    margin-right: 15%;
+    padding-top: 2%;
+    padding-right: 18em;
+    padding-left: 4%;
   }
 
   .btnLogin{
@@ -170,12 +231,32 @@
     background: #353880;
     color:white;
   }
+
+  .footer{
+    background-color: #353880;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    padding-bottom: 0.3%;
+    width: 100%;
+    color: white;
+    text-align: center;
+  }
+
+  .textoFooter{
+    /* display:inline-block; */
+		vertical-align:middle;
+		/* line-height:normal; */
+    font-size: 80%;
+    cursor: pointer;
+  }
 </style>
 
 <script>
   import { validUsername } from '@/utils/validate'
   import logSuper from '@/assets/superservicios1.png'
   import logTarifarito from '@/assets/logo_buho.png'
+  import logTarifarito1 from '@/assets/logociad.png'
 
   export default {
     name: 'Login',
@@ -333,7 +414,7 @@
     }
 
     .el-form-item {
-      border: 1px solid rgba(255, 255, 255, 0.1);
+      border: 0px solid rgba(255, 255, 255, 0.1);
       background: rgba(0, 0, 0, 0.1); //color de fondo del input
       border-radius: 5px;
       color: #454545;
@@ -347,7 +428,8 @@
   $light_gray:#eee;
 
   .login-container {
-    min-height: 100%;
+    border: 0px solid red;
+    height: 100%;
     width: 100%;
     // background-color: $bg;
     background: $bg;
