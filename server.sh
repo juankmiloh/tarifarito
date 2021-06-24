4dffbc71db466e221e8b37af8a1db27c5421c6d5
juankmiloh <juankmiloh@hotmail.com>
2021-06-23 20:40:55 -0500

    Actualizando conexion al servidor de la SSPD

---
+++ server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# echo run server de produccion frontend
+# -- COMANDOS PARA LEVANTAR SERVIDOR DE PRODUCCION VUEJES
+# 1. sudo npm install -g serve
+# 2. npm run build:prod
+serve -s dist -l 5058
